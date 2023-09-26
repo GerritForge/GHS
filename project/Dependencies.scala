1fa86673f522fe0f1c72ddc98d4be189b3fa9974
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-09-26 16:22:04 +0100

    Add quartz scheduler library [1]
    [1] https://github.com/quartz-scheduler/quartz/
    
    Change-Id: I8a7f89a6fb1c5be985c4755a8e1432591bc2ae0b

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,6 @@
 import sbt._
 
 object Dependencies {
-  lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
+  lazy val quartz = "org.quartz-scheduler" % "quartz" % "2.3.2"
+  lazy val munit  = "org.scalameta"       %% "munit"  % "0.7.29"
 }
