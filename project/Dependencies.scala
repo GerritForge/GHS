0a376e34f86b5379a5119c4deca6f3069f7587af
Darius Jokilehto <dariusjokilehto@gmail.com>
2023-09-26 22:00:10 +0100

    Add Metrics ADT and PureConfig deserialisers
    Change-Id: I2af9bae42f05ca95357a04748d60506e167e9765

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,8 @@ import sbt._
 import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object Dependencies {
-  lazy val quartz = "org.quartz-scheduler" % "quartz" % "2.3.2"
-  lazy val munit  = "org.scalameta"       %% "munit"  % "0.7.29"
-  lazy val kamon = "io.kamon" %% "kamon-prometheus" % "2.5.9"
+  lazy val kamon      = "io.kamon"              %% "kamon-prometheus" % "2.5.9"
+  lazy val munit      = "org.scalameta"         %% "munit"            % "0.7.29"
+  lazy val pureConfig = "com.github.pureconfig" %% "pureconfig"       % "0.17.4"
+  lazy val quartz     = "org.quartz-scheduler"   % "quartz"           % "2.3.2"
 }
