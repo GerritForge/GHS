4582c4426cfd3fdccdb2e7ed0f1f0bc7dd8fd6a1
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-09-26 17:23:44 +0100

    Add logback and scala-logging dependencies
    Change-Id: Ibcbe74a6090e716bb61080d1e133cd972e669448

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,8 @@
 import sbt._
 
 object Dependencies {
-  lazy val quartz = "org.quartz-scheduler" % "quartz" % "2.3.2"
-  lazy val munit  = "org.scalameta"       %% "munit"  % "0.7.29"
+  lazy val logback      = "ch.qos.logback"              % "logback-classic" % "1.4.11"
+  lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.4"
+  lazy val quartz       = "org.quartz-scheduler"        % "quartz"          % "2.3.2"
+  lazy val munit        = "org.scalameta"              %% "munit"           % "0.7.29"
 }
