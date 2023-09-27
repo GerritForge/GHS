87a14567346224d3410be0c7247a1c7e80860400
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-09-27 17:16:13 +0100

    Add jgit dependency
    Change-Id: Id67d9a84ee07c521e5ef2e01579e1f01bd78e404

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 object Dependencies {
+  lazy val jgit         = "org.eclipse.jgit"            % "org.eclipse.jgit" % "6.7.0.202309050840-r"
   lazy val kamon        = "io.kamon"                   %% "kamon-prometheus" % "2.5.9"
   lazy val logback      = "ch.qos.logback"              % "logback-core"     % "1.4.11"
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging"    % "3.9.5"
