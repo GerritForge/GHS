158657cf4727a50cb5fd2352d75976a4eb5d8052
Darius Jokilehto <dariusjokilehto@gmail.com>
2023-09-27 12:51:52 +0100

    Read tasks.conf into config
    Also remove MetricsConf to make way for a single config class that reads
    in both the metrics and and tasks configs.
    
    Change-Id: I63acbc3a6886685f7e864e1d41190b660ac603a8

--- src/main/scala/com/gerritforge/ghs/config/MetricsConf.scala
+++
@@ -1,9 +0,0 @@
-package com.gerritforge.ghs.config
-
-import scala.concurrent.duration.FiniteDuration
-
-final case class MetricsConf(projects: List[MetricsConf.Project])
-
-object MetricsConf {
-  final case class Project(name: String, frequency: FiniteDuration)
-}
