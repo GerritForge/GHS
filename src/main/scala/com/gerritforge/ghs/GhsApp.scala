fa3847782a9a32b338df3d8483ec4a748f08a8c1
Fabio Ponciroli <ponch78@gmail.com>
2023-09-27 16:58:51 -0700

    Move away scheduling logic from sample App
    Change-Id: Ie399a834a3adc9993744750fae340ee172e99f67

--- src/main/scala/com/gerritforge/ghs/GhsApp.scala
+++ src/main/scala/com/gerritforge/ghs/GhsApp.scala
@@ -1,11 +1,47 @@
 package com.gerritforge.ghs
 
+import com.gerritforge.ghs.config.MetricsConf
+import com.gerritforge.ghs.metrics.{CollectionJob, ContextMetrics, SchedulerUtilities}
 import kamon.Kamon
+import org.quartz.impl.StdSchedulerFactory
+
+import java.io.File
+import java.util.Properties
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
 
 object GhsApp extends App {
   Kamon.init()
-  while (true) {
-    Thread.sleep(1000)
-    Kamon.counter("hello.GHS").withoutTags().increment()
-  }
+
+  val properties = new Properties()
+  properties.put("org.quartz.threadPool.threadCount", "10")
+  val scheduler = new StdSchedulerFactory(properties).getScheduler
+
+  scheduler.start()
+
+  // TODO this is for testing purposes
+  val metricsProjects =
+    Seq(
+      MetricsConf.Project(new File("TODO_ADD"), 5.seconds)
+    )
+
+  metricsProjects
+    .foreach { project =>
+      scheduler.scheduleJob(
+        SchedulerUtilities
+          .JobDetails[CollectionJob.JGit](
+            ContextMetrics.JGit.`type`,
+            project.path.getAbsolutePath,
+            ContextMetrics.JGit.data(project)
+          ),
+        SchedulerUtilities
+          .Trigger(ContextMetrics.JGit.`type`, project)
+      )
+    }
+
+  // XXX: Add hook to capture sigint
+  println("Starting to sleep...")
+  Thread.sleep(240000)
+  scheduler.shutdown()
+  Await.result(Kamon.stop(), 10.seconds)
 }
