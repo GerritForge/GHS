e84c49b4de4453adc39ba38eff2429dfafdeeebb
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-09-27 08:45:09 +0100

    Run scalafmtSbt to format sbt files
    Change-Id: I63162701b1e9960ace8f51651d5d40fd2f704fb4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val quartz = "org.quartz-scheduler" % "quartz" % "2.3.2"
-  lazy val munit  = "org.scalameta"       %% "munit"  % "0.7.29"
-  lazy val kamon = "io.kamon" %% "kamon-prometheus" % "2.5.9"
+  lazy val quartz = "org.quartz-scheduler" % "quartz"           % "2.3.2"
+  lazy val munit  = "org.scalameta"       %% "munit"            % "0.7.29"
+  lazy val kamon  = "io.kamon"            %% "kamon-prometheus" % "2.5.9"
 }
