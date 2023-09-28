2bac1c72b62dd27e84becdc0b1fc7933defba2c5
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-09-28 11:01:12 +0100

    Change Project case class to accomodate gerrit site
    Change-Id: I569ef24e8762f5d1ead185727e0b3c51bf1c8d69

--- src/main/scala/com/gerritforge/ghs/config/MetricsConf.scala
+++ src/main/scala/com/gerritforge/ghs/config/MetricsConf.scala
@@ -1,9 +1,10 @@
 package com.gerritforge.ghs.config
 
+import java.io.File
 import scala.concurrent.duration.FiniteDuration
 
-final case class MetricsConf(projects: List[MetricsConf.Project])
+final case class MetricsConf(gerritSite: File, projects: List[MetricsConf.Project])
 
 object MetricsConf {
-  final case class Project(name: String, frequency: FiniteDuration)
+  final case class Project(name: File, frequency: FiniteDuration)
 }
--- src/test/scala/com/gerritforge/ghs/config/MetricsConfSuite.scala
+++ src/test/scala/com/gerritforge/ghs/config/MetricsConfSuite.scala
@@ -6,12 +6,19 @@ import org.scalatest.matchers.should.Matchers
 import pureconfig._
 import pureconfig.generic.auto._
 
+import java.io.File
 import scala.concurrent.duration._
 
 class MetricsConfSuite extends AnyFlatSpec with Matchers {
   "MetricsConf" should "read metrics config" in {
-    val result   = ConfigSource.resources("collection.conf").load[MetricsConf]
-    val expected = MetricsConf(List(Project("project1", 1.hour), Project("project2", 1.minute)))
+    val result = ConfigSource.resources("collection.conf").load[MetricsConf]
+    val expected = MetricsConf(
+      gerritSite = new File("/directory1/directory2"),
+      projects = List(
+        Project(new File("project1"), 1.hour),
+        Project(new File("project2"), 1.minute)
+      )
+    )
     result shouldBe Right(expected)
   }
 }
