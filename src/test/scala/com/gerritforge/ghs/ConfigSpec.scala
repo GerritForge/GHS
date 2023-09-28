4cb6ccaaeecdc1ddf03172d0e21fb3fbc125ff2c
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-09-28 11:01:12 +0100

    Change Project case class to accomodate git site
    Change-Id: I569ef24e8762f5d1ead185727e0b3c51bf1c8d69

--- src/test/scala/com/gerritforge/ghs/ConfigSpec.scala
+++ src/test/scala/com/gerritforge/ghs/ConfigSpec.scala
@@ -4,12 +4,14 @@ import com.gerritforge.ghs.Config.{MetricsCollection, Tasks}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import java.io.File
 import scala.concurrent.duration._
 
 class ConfigSpec extends AnyFlatSpec with Matchers {
   "PureConfig" should "read a valid config" in {
     val expectedMetrics = MetricsCollection(
-      List(MetricsCollection.Project("project1", 1.hour), MetricsCollection.Project("project2", 1.minute))
+      gitSiteBasePath = new File("/directory1/directory2"),
+      projects = List(MetricsCollection.Project("project1", 1.hour), MetricsCollection.Project("project2", 1.minute))
     )
     val expectedTasks =
       Tasks(List(Tasks.Project("project1", "rm", "-fr"), Tasks.Project("project2", "jgit gc", "--prune=now")))
