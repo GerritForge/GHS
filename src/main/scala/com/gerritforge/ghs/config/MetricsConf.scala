d590e0bf96526c03d9284cca0a5f95ebe6d0ac9f
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-09-28 11:01:12 +0100

    Change Project case class to accomodate the path
    Change-Id: I569ef24e8762f5d1ead185727e0b3c51bf1c8d69

--- src/main/scala/com/gerritforge/ghs/config/MetricsConf.scala
+++ src/main/scala/com/gerritforge/ghs/config/MetricsConf.scala
@@ -1,9 +1,15 @@
 package com.gerritforge.ghs.config
 
+import java.io.File
 import scala.concurrent.duration.FiniteDuration
 
 final case class MetricsConf(projects: List[MetricsConf.Project])
 
 object MetricsConf {
-  final case class Project(name: String, frequency: FiniteDuration)
+  final case class Project(path: File, frequency: FiniteDuration) {
+    require(
+      path.isDirectory,
+      s"$path should be a directory"
+    )
+  }
 }
--- src/test/scala/com/gerritforge/ghs/config/MetricsConfSuite.scala
+++ src/test/scala/com/gerritforge/ghs/config/MetricsConfSuite.scala
@@ -1,15 +1,66 @@
 package com.gerritforge.ghs.config
 
 import com.gerritforge.ghs.config.MetricsConf.Project
+import com.typesafe.config.ConfigFactory
 import pureconfig._
 import pureconfig.generic.auto._
 
+import java.io.File
+import java.nio.file.Files
 import scala.concurrent.duration._
 
 class MetricsConfSuite extends munit.FunSuite {
   test("pureConfig should read metrics config") {
-    val result   = ConfigSource.resources("collection.conf").load[MetricsConf]
-    val expected = MetricsConf(List(Project("project1", 1.hour), Project("project2", 1.minute)))
+    val project1Folder = Files.createTempDirectory("project1").toFile.getAbsolutePath
+    val project2Folder = Files.createTempDirectory("project2").toFile.getAbsolutePath
+    val collectionConfiguration =
+      s"""
+        |projects = [
+        |    {
+        |        path = $project1Folder
+        |        frequency = 1h
+        |    }
+        |    {
+        |        path = $project2Folder
+        |        frequency = 1m
+        |    }
+        |]
+        |""".stripMargin
+
+    val result = ConfigSource
+      .fromConfig(ConfigFactory.parseString(collectionConfiguration))
+      .load[MetricsConf]
+
+    val expected = MetricsConf(
+      List(
+        Project(new File(project1Folder), 1.hour),
+        Project(new File(project2Folder), 1.minute)
+      )
+    )
     assertEquals(result, Right(expected))
+  }
+
+  test("pureConfig should fails when project is not a directory") {
+    val project1AsFile = Files.createTempFile("prefix", ".txt").toFile.getAbsolutePath
+    val collectionConfiguration =
+      s"""
+         |projects = [
+         |    {
+         |        path = ${project1AsFile}
+         |        frequency = 1h
+         |    }
+         |]
+         |""".stripMargin
+
+    val exception = intercept[IllegalArgumentException](
+      ConfigSource
+        .fromConfig(ConfigFactory.parseString(collectionConfiguration))
+        .load[MetricsConf]
+    )
+
+    assertEquals(
+      exception.getMessage,
+      s"requirement failed: ${project1AsFile} should be a directory"
+    )
   }
 }
