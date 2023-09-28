b9bb795dca4e757ddf7f62e22fff641b492b22d9
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-09-28 11:01:12 +0100

    Change Project case class to accomodate the path
    Change-Id: I569ef24e8762f5d1ead185727e0b3c51bf1c8d69

--- src/test/scala/com/gerritforge/ghs/config/MetricsConfSuite.scala
+++ src/test/scala/com/gerritforge/ghs/config/MetricsConfSuite.scala
@@ -4,12 +4,18 @@ import com.gerritforge.ghs.config.MetricsConf.Project
 import pureconfig._
 import pureconfig.generic.auto._
 
+import java.io.File
 import scala.concurrent.duration._
 
 class MetricsConfSuite extends munit.FunSuite {
   test("pureConfig should read metrics config") {
-    val result   = ConfigSource.resources("collection.conf").load[MetricsConf]
-    val expected = MetricsConf(List(Project("project1", 1.hour), Project("project2", 1.minute)))
+    val result = ConfigSource.resources("collection.conf").load[MetricsConf]
+    val expected = MetricsConf(
+      List(
+        Project(new File("project1"), 1.hour),
+        Project(new File("project2"), 1.minute)
+      )
+    )
     assertEquals(result, Right(expected))
   }
 }
