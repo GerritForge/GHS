a2ab71d4f53f1dab5fd6687302adabb9c3638da1
Darius Jokilehto <dariusjokilehto@gmail.com>
2023-09-30 11:18:26 +0200

    Remove project name from Task
    The Task model is read from config. It currently includes the project
    name, meaning we need one task per project, even though the task is the
    same.
    
    This change removes the the project name from the task config, and
    accepts it as an input from the HTTP server. This allows dynamic
    arguments to the command that is to be executed, which can be provided
    by the rules engine, for example. It also allows us to reuse a single
    task definition across different projects.
    
    Change-Id: I9d6d476320cf72b9bd11ffabb2da499f27f31ff5

--- src/main/scala/com/gerritforge/ghs/task/http/Server.scala
+++ src/main/scala/com/gerritforge/ghs/task/http/Server.scala
@@ -1,7 +1,6 @@
 package com.gerritforge.ghs.task.http
 
-import com.gerritforge.ghs.task.Tasks
-import com.gerritforge.ghs.task.Tasks.Project
+import com.gerritforge.ghs.task.Task
 import sttp.model.StatusCode
 import sttp.tapir._
 import sttp.tapir.server.netty.{NettyFutureServer, NettyFutureServerBinding}
@@ -10,20 +9,23 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 object Server {
-  val tasksPath = endpoint.in("tasks")
+  def taskEndpoint(task: Task) = {
+    val queryInput = task.inputs.foldLeft[EndpointInput[_]](emptyInput) { case (acc, s) =>
+      acc and query[String](s)
+    }
 
-  def tasksProjectEndpoint(project: Project) =
-    tasksPath
-      .in(project.name)
-      .post
+    endpoint.post
+      .in("tasks" / task.name)
+      .in(queryInput)
       .out(emptyOutput and statusCode(StatusCode.Accepted))
       .serverLogic { _ =>
-        println(s"Call ${project.command} ${project.parameters}")
+        println(s"Call ${task.command} ${task.parameters}")
         Future.successful[Either[Unit, Unit]](Right(()))
       }
+  }
 
-  def binding(tasks: Tasks): Future[NettyFutureServerBinding] = {
-    val endpoints = tasks.projects.map(tasksProjectEndpoint)
+  def binding(tasks: List[Task]): Future[NettyFutureServerBinding] = {
+    val endpoints = tasks.map(taskEndpoint)
     NettyFutureServer().addEndpoints(endpoints).start()
   }
 }
