959f006917e18efcd49910be86f32b60a2a8c980
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-10-01 18:10:37 +0100

    Provide ADT to encode the type of job
    This update introduces an Algebraic Data Type (ADT)
    called Metric, which encapsulates the job type as
    either Jgit or FileSystem. This enhancement enhances
    the job definition, associating the job's name with
    the project's name and the job group with its type.
    
    Change-Id: Ic0120762d000ce32537847a9278a872189d9bdfb

--- src/main/scala/com/gerritforge/ghs/metrics/CollectionJob.scala
+++ src/main/scala/com/gerritforge/ghs/metrics/CollectionJob.scala
@@ -3,6 +3,6 @@ package com.gerritforge.ghs.metrics
 import org.quartz.{Job, JobExecutionContext}
 final class CollectionJob extends Job {
   override def execute(context: JobExecutionContext): Unit = {
-    println("do something")
+    println(s"Job with name: ${context.getJobDetail.getKey.getName} and group: ${context.getJobDetail.getKey.getGroup}")
   }
 }
--- src/main/scala/com/gerritforge/ghs/metrics/CollectionScheduler.scala
+++ src/main/scala/com/gerritforge/ghs/metrics/CollectionScheduler.scala
@@ -4,21 +4,35 @@ import com.gerritforge.ghs.Config.MetricsCollection
 import org.quartz._
 
 import java.util.Date
+
+sealed trait Metric {
+  def `type`: String
+}
+
+object Metric {
+  case object JGit extends Metric {
+    override val `type` = "jgit"
+  }
+  case object FileSystem extends Metric {
+    override val `type` = "fs"
+  }
+}
+
 final class CollectionScheduler(scheduler: Scheduler) {
-  def schedule(project: MetricsCollection.Project): Date = {
-    scheduler.scheduleJob(buildJobDetail(project), buildTrigger(project))
+  def schedule(project: MetricsCollection.Project, metric: Metric): Date = {
+    scheduler.scheduleJob(buildJobDetail(project, metric), buildTrigger(project, metric))
   }
 
-  private def buildJobDetail(project: MetricsCollection.Project): JobDetail = {
+  private def buildJobDetail(project: MetricsCollection.Project, metric: Metric): JobDetail = {
     JobBuilder
       .newJob(classOf[CollectionJob])
-      .withIdentity(project.name)
+      .withIdentity(project.name, metric.`type`)
       .build
   }
 
-  private def buildTrigger(project: MetricsCollection.Project): Trigger =
+  private def buildTrigger(project: MetricsCollection.Project, metric: Metric): Trigger =
     TriggerBuilder.newTrigger
-      .withIdentity(project.name)
+      .withIdentity(project.name, metric.`type`)
       .withSchedule(
         SimpleScheduleBuilder.simpleSchedule
           .withIntervalInSeconds(project.frequency.toSeconds.toInt)
